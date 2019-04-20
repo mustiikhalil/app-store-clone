60e93338a057197f466be24fe2c028f562bbc36b
mustii <mustii@mmk.one>
2019-04-21 01:55:05 +0300

    Added the main operations

---
+++ app_store/Operations/FetchOperation.swift
@@ -0,0 +1,9 @@
+//
+//  FetchOperation.swift
+//  app_store
+//
+//  Created by Mustafa Khalil on 4/21/19.
+//  Copyright © 2019 Mustafa Khalil. All rights reserved.
+//
+
+import Foundation
---
+++ app_store/Operations/OperationData.swift
@@ -0,0 +1,9 @@
+//
+//  OperationData.swift
+//  app_store
+//
+//  Created by Mustafa Khalil on 4/21/19.
+//  Copyright © 2019 Mustafa Khalil. All rights reserved.
+//
+
+import Foundation
