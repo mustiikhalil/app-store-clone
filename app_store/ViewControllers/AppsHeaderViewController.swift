05f50cf7512d5aa48398a2af3c2a35d766cd649d
mustii <mustii@mmk.one>
2019-05-22 23:36:04 +0300

    Added the header cell

--- app_store/ViewControllers/AppsHeaderViewController.swift
+++ app_store/ViewControllers/AppsHeaderViewController.swift
@@ -14,7 +14,7 @@ class AppsHeaderViewController: UICollectionViewController, UICollectionViewDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(AppsHeaderCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -22,14 +22,58 @@ class AppsHeaderViewController: UICollectionViewController, UICollectionViewDele
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .yellow
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! AppsHeaderCell
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 16, bottom: 0, right: 0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        return .init(width: view.frame.width, height: view.frame.height)
+        return .init(width: view.frame.width - 48, height: view.frame.height)
     }
     
+}
+
+class AppsHeaderCell: UICollectionViewCell {
+    
+    let companyLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "Facebook"
+        lbl.textColor = .blue
+        lbl.font = UIFont.systemFont(ofSize: 12)
+        return lbl
+    }()
+    
+    let titleLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.text = "Title"
+        lbl.numberOfLines = 2
+        lbl.font = UIFont.systemFont(ofSize: 24)
+        return lbl
+    }()
+    
+    let imageView: UIImageView = {
+        let img = UIImageView()
+        img.translatesAutoresizingMaskIntoConstraints = false
+        img.layer.cornerRadius = 8
+        img.clipsToBounds = true
+        img.backgroundColor = .red
+        return img
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let stackView = VerticalStackView(arrangedSubviews: [companyLabel, titleLabel, imageView], spacing: 12)
+        addSubview(stackView)
+        stackView.fillSuperView(padding: .init(top: 16, left: 0, bottom: 0, right: 0))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
